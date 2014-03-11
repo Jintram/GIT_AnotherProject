2f8b83d06853ed7b48b5a9c7cb181bf365668593
Martijn Wehrens <spam@Jintram.nl>
2014-03-11 10:53:43 +0100

    First feature

---
+++ feature1.py
@@ -0,0 +1,3 @@
+def multiwow(a,b):
+    c = a*b
+    return c
---
+++ hello.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 10 17:12:17 2014
+
+@author: wehrens
+"""
+
+print "Hello"
+
+from feature1 import *
+
+print "Aaand 2*3 = "+str(multiwow(2,3))
+
+
+
+
+
+
+
+
+
+
+
+
+
